5e3167a103fbafd137149dd9b96499731e286948
Matthew Deig <mattwdeig@gmail.com>
2014-07-29 17:29:36 -0500

    switch to python becuase I know it better

---
+++ src/messages/pyboard.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#this is going to be a message board for the bbs
+#that I'm working on
+#Programmer Matthew Deig
+
+import os
+import dataset
+from blessings import Terminal
+
