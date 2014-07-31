e2353d1815003763b7d117dabcce4919db427b66
Matthew Deig <mattwdeig@gmail.com>
2014-07-31 10:49:41 -0500

    viewable replies woot

--- src/messages/pyboard.py
+++ src/messages/pyboard.py
@@ -38,10 +38,39 @@ def showMessage(db, gid, mid, term):
     for topic in topics:
         print(term.green + "Author: " + term.normal + topic['msgStarter'])
         print(term.green + "Topic: " + term.normal + topic['subject'])
+        print(term.green + "Date: " + term.normal + topic['date'])
         print("------------------------")
         print()
         print()
         print(topic['message'])
+
+def readReply(db, gid, mid, rid, term):
+    topicMsgs = db['message']
+
+    
+    topics = topicMsgs.find(gid=gid, mid=mid)
+
+    msgTitle = ""
+    msgStarter = ""
+    for topic in topics:
+        msgTitle = topic['subject']
+        msgStarter = topic['msgStarter']
+
+    replyMsgs = db['reply']
+    replys = replyMsgs.find(mid=mid, rid=rid)
+
+    for reply in replys:
+        print(term.green + "Orginal Author: " + term.normal + msgStarter)
+        print(term.green + "Reply Author: " + term.normal + reply['rpyUser'])
+        print(term.green + "RE: " + term.normal + msgTitle)
+        print(term.green + "Date: " + term.normal + reply['date'])
+        print("------------------------")
+        print()
+        print()
+        print(reply['message'])
+
+
+
 
 def showHelp():
     pass
@@ -53,10 +82,14 @@ def run(term):
 
     location = "none"
     pLocation = "None"
+    gid = 0
+    mid = 0
+    rid = 0
 
     while True:
         print(term.clear)
         print(term.blue + term.bold + "Welcome to the Message Boards"+term.normal)
+        print()
         
         if location == "none":
             showGroups(db)
@@ -65,7 +98,13 @@ def run(term):
         elif location == "group":
             showGroup(db, gid, term)
         elif location == "message":
+            rid = 0
             showMessage(db, gid, mid, term)
+        elif location == "readReply":
+            readReply(db, gid, mid, rid, term)
+            rid = rid + 1
+            location = "message"
+
 
         print()
         print()
@@ -97,6 +136,11 @@ def run(term):
             elif location == "group":
                 location = "none"
                 pLocation = "None"
+            elif rid > 0:
+                location = "message"
+        elif select == 'N' or select == 'n' and location == "message":
+            location = "readReply"
+
 
 
 
